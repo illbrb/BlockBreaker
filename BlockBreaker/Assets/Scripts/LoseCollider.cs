048a9739adbb350d3f1e5d5b22e84d66f0b9ffe6
illbrb <illbrbgit@gmail.com>
2016-04-25 21:55:13 +0100

    chk

---
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManger;
+	
+	
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		print ("Trigger");
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		print ("Collision");
+		levelManger.LoadLevel("Win");
+	}
+	
+}
---
+++ BlockBreaker/Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		Vector3 paddlePosition = new Vector3 (transform.position.x,transform.position.y,0f);
+		paddlePosition.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+		transform.position = paddlePosition;
+		print (mousePosInBlocks);
+
+	
+	}
+}
